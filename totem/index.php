5f46fc04a96b4deec572f130e42779f5321e6f29
Nicolailemos <nicolaihenriquedeve@gmail.com>
2022-10-28 17:24:59 -0300

    terminei o index e pergunta1

--- totem/index.php
+++ totem/index.php
@@ -4,32 +4,67 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
-    <link href="css/style.css" rel="stylesheet" />
+
+    <style>
+        body {}
+
+        .container {
+            border: 1px solid black;
+
+        }
+
+        .pesquisa {
+            width: 70%;
+            padding: 12px;
+
+        }
+
+        .button {
+            width: 300px;
+            padding: 14px;
+        }
+    </style>
+
 </head>
 
 <body>
 
+    <div class="row">
+        <div class="col-sm-12">
+            <nav class="navbar bg-primary">
+                <div class="container-fluid">
+                    <img class="img-floid mx-auto d-block" src="https://www.mundosenai.com.br/media/filer_public/8e/b4/8eb4a5a5-3946-46ed-b2be-04b18a4b7c94/logo-senai.png" alt="logo">
+                </div>
+            </nav>
+        </div>
+    </div>
 
-    <div class="">
+    <br><br><br><br><br>
+
+    <div class="container border">
         <form action="/action_page.php">
             <div class="row">
-                <h2 style="text-align:center">Pesquisa de satisfação </h2>
-
-
-                <div class="col">
-                    <img class="img-fluid" src="img/telainicial.png" alt="" />
+                <div class="col-12 ">
+                    <h2 class="text-center">Pesquisa de satisfação </h2>
                 </div>
+            </div>
 
-                <div class="col">
-                    <a href="pergunta1.php"> <input class="pesq" type="button" value="Pesquisa de satisfação"> </a>
+            <div class="row">
+                <div class="col-sm-6 col-md-6 col-lg-6">
+                    <img class="img-fluid " src="img/telainicial.png" alt="" />
+                </div>
+                <div class="col-sm-6 col-md-6 col-lg-6 text-center mt-5">
+                    <a href="pergunta1.php"> <input class="btn btn-primary pesquisa" type="button" value="Pesquisa de satisfação"> </a>
+
                     <h1> Conheça Também :</h1>
-                    <input class="pesq1" type="button" value="EDUConnect">
-                    <input class="pesq1" type="button" value="Mundo Senai">
+
+                    <input class="btn btn-primary button m-2" type="button" value="EDUConnect">
+                    <input class="btn btn-primary button" type="button" value="Mundo Senai">
                 </div>
 
             </div>
-        </form>
     </div>
 
 </body>
--- totem/pergunta1.php
+++ totem/pergunta1.php
@@ -4,12 +4,30 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
 
     <style>
-    .senai {
+        body{
+           
+        }
+
+        .pergunta {
+            border-radius: 1080px;
+            width: 600px;
+        }
+
+        .container {
+            background-color: #719dc7;
+        }
+
+        .boneco {
+        width: 500px;
+        margin-top: -100px;
+     
+    }
+
+        /*  .senai {
         width: 450px;
         margin-left: 700px;
         margin-top: 10px;
@@ -21,20 +39,6 @@
         margin-left: 70px;
     }
 
-    .container {
-        background-color: #719dc7;
-        margin-top: 50px;
-
-    }
-
-    .pergunta {
-        border-radius: 1080px;
-        margin-left: 320px;
-        margin-top: 20px;
-        width: 600px;
-    }
-
-
     .button {
         font-size: 18px;
         padding: 8px;
@@ -42,19 +46,14 @@
         margin: 10px;
         margin-top: -230px;
         border: none;
-        
-
     }
 
     .button1 {
         margin-left: 470px;
-        
-
     }
 
     .button1:hover {
       background-color: #15ff00;
-
     }
 
     .button2:hover {
@@ -64,38 +63,44 @@
     .button3:hover {
       background-color: #ff0000;
     }
-
-
-
-
+ */
     </style>
 
 </head>
 
 <body>
 
+    <div class="row">
+        <div class="col-sm-12">
+            <nav class="navbar bg-primary">
+                <div class="container-fluid">
+                    <img class="img-floid mx-auto d-block" src="https://www.mundosenai.com.br/media/filer_public/8e/b4/8eb4a5a5-3946-46ed-b2be-04b18a4b7c94/logo-senai.png" alt="logo">
+                </div>
+            </nav>
+        </div>
+    </div>
 
-    <img class="senai" src="img/senai.jpg" alt="senai">
+    <div class="container">
+        <form action="/action_page.php" method="post">
+            <div class="row">
+                <div class="col-12 text-center mt-2">
+                    <img src="img/p1.jpeg" alt="Avatar" class="pergunta">
+                </div>
+            </div>
 
-
-    <form action="/action_page.php" method="post">
-
-        <div class="container">
-            <img src="img/p1.jpeg" alt="Avatar" class="pergunta">
-        
-
-    </form>
-
-    <img class="boneco" src="img/telapergunta.png" alt="Avatar">
-
-    <br>
-
-    <a href="pergunta2.php" class="btn btn-primary button button1">Ótimo</a>
-    <a href="pergunta2.php" class="btn btn-primary button button2">Bom</a>
-    <a href="pergunta2.php" class="btn btn-primary button button3">Regular</a>
+            <div class="row">
+                <div class="col-12 text-center mt-2">
+                    <a href="pergunta2.php" class="btn btn-primary button button1">Ótimo</a>
+                    <a href="pergunta2.php" class="btn btn-primary button button2">Bom</a>
+                    <a href="pergunta2.php" class="btn btn-primary button button3">Regular</a>
+                </div>
+            </div>
+            
+            <img class="boneco" src="img/telapergunta.png" alt="Avatar">
     </div>
 
 
+  
 </body>
 
 </html>
